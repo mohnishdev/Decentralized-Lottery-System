2ae4c054a27ef5d6d89809ec5bd15ea4e716952a
Mohnish Sharma <mohnish-nsm@Mohnishs-McBook.local>
2024-02-20 15:39:52 +0530

    First Setup commit

---
+++ src/Raffle.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+/**
+ * @title A sample Raffle Contract
+ * @author Mohnish Sharma
+ * @notice This contract is for creating simple lottery system called Raffle
+ */
+
+contract Raffle {
+
+    uint256 private s_entranceFee;
+
+    constructor
+
+    function enterRaffle() public payable{
+
+    }
+
+    function pickWinner() public {
+
+    }
+}
